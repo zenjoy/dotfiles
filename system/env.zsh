dc957a72b5a5c4face069cfb380e908a05313798
dedene <peter@dedene.com>
2018-08-09 09:25:47 +0200

    Changing default editor to Visual Studio Code
    Former-commit-id: e1127ba790fbcbe9f098adb82270624a63de251f

--- system/env.zsh
+++ system/env.zsh
@@ -1,7 +1,7 @@
 # Only set this if we haven't set $EDITOR up somewhere else previously.
 if [[ "$EDITOR" == "" ]] ; then
   # Use sublime for my editor.
-  export EDITOR='subl'
+  export EDITOR='code'
 fi
 
 
