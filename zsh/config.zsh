59d198fafda405f35d098f2f43ca242aabf6ef68
Peter Dedene <peter@zenjoy.be>
2019-03-05 10:39:20 +0100

    Add syntax highlighting to our setup

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -9,27 +9,44 @@ HISTFILE=~/.zsh_history
 HISTSIZE=10000
 SAVEHIST=10000
 
-setopt NO_BG_NICE # don't nice background tasks
-setopt NO_HUP
-setopt NO_LIST_BEEP
-setopt LOCAL_OPTIONS # allow functions to have local options
-setopt LOCAL_TRAPS # allow functions to have local traps
-setopt HIST_VERIFY
-setopt SHARE_HISTORY # share history between sessions ???
-setopt EXTENDED_HISTORY # add timestamps to history
-setopt PROMPT_SUBST
-setopt CORRECT
-setopt COMPLETE_IN_WORD
-setopt IGNORE_EOF
-
-setopt APPEND_HISTORY # adds history
-setopt INC_APPEND_HISTORY SHARE_HISTORY  # adds history incrementally and share it across sessions
-setopt HIST_IGNORE_ALL_DUPS  # don't record dupes in history
-setopt HIST_REDUCE_BLANKS
-
-# don't expand aliases _before_ completion has finished
-#   like: git comm-[tab]
-setopt complete_aliases
+setopt no_bg_nice               # don't nice background tasks
+setopt no_hup                   # no hup signal at shell exit
+setopt no_beep                  # no annoying beep when a completion is ambiguous
+setopt no_list_beep             # beeping is only turned off for ambiguous completions
+setopt local_options            # allow functions to have local options
+setopt local_traps              # allow functions to have local traps
+setopt share_history            # share history between sessions ???
+setopt extended_history         # add timestamps to history
+setopt prompt_subst             # parameter expansion, command substitution and arithmetic expansion are performed in prompts
+setopt correct                  # try to correct the spelling of commands
+setopt complete_in_word         # allow completion from within a word/phrase
+setopt hash_list_all            # hash everything before completion
+setopt complete_aliases         # don't expand aliases _before_ completion has finished
+setopt always_to_end            # when completing from the middle of a word, move the cursor to the end of the word
+setopt list_ambiguous           # complete as much of a completion until it gets ambiguous.
+setopt ignore_eof               # do not exit on end-of-file
+setopt append_history           # adds history
+setopt inc_append_history       # adds history incrementally and share it across sessions
+setopt share_history            # share hist between sessions
+setopt hist_ignore_all_dups     # don't record dupes in history
+setopt hist_reduce_blanks       # trim blanks
+setopt bang_hist                # !keyword
+setopt hist_verify              # show before executing history commands
+unsetopt hist_ignore_space      # ignore space prefixed commands
+setopt auto_cd                  # if command is a path, cd into it
+setopt auto_remove_slash        # self explicit
+setopt chase_links              # resolve symlinks
+setopt correct                  # try to correct spelling of commands
+setopt extended_glob            # activate complex pattern globbing
+setopt glob_dots                # include dotfiles in globbing
+setopt auto_pushd               # make cd push old dir in dir stack
+setopt pushd_ignore_dups        # no duplicates in dir stack
+setopt pushd_silent             # no dir stack after pushd or popd
+setopt pushd_to_home            # `pushd` = `pushd $HOME`
+unsetopt correct_all            # do not try to spelling correct commands
+unsetopt rm_star_silent         # ask for confirmation for `rm *' or `rm path/*'
+unsetopt bg_nice                # no lower prio for background jobs
+unsetopt hist_beep              # no bell on error in history
 
 zle -N newtab
 
