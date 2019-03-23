6e1276b0e07e053d3d2f6d1503abb2fcb9d6e7ed
Peter Dedene <peter@zenjoy.be>
2019-03-23 13:49:14 +0100

    Make the hub git alias work again

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,9 +1,7 @@
 # Use `hub` as our git wrapper:
 #   http://defunkt.github.com/hub/
-hub_path=$(which hub)
-if (( $+commands[hub] ))
-then
-  alias git=$hub_path
+if [ -f /usr/local/rbenv/shims/hub ]; then
+  alias git="hub"
 fi
 
 # The rest of my fun git aliases
