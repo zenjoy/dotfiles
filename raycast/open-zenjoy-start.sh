1889dee46085a372495acd67783f5527842cb14a
Peter Dedene <peter@zenjoy.be>
2021-07-08 16:23:41 +0200

    Add many useful raycast scripts

---
+++ raycast/open-zenjoy-start.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Open Zenjoy Start
+# @raycast.packageName Bookmarks
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🏡
+
+# Documentation:
+# @raycast.description Open the Zenjoy Tools start page
+
+open https://start.zenjoy.be
